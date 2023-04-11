bc26f749f09c424a031bc61a3158997d17ee340a
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-11 11:17:10 -0700

    Tests passing.  Beginning work on live interaction with YouTube.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,15 +1,17 @@
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => {
+  // console.log(props.children);
+  //console.log(props);
+  // <div key={video.id.videoId}><h5><em>videoListEntry</em><VideoListEntry video={video} /></h5></div>
+
+  var vList = props.videos.map((video) => <VideoListEntry key = {video.id.videoId} video = {video} handleClick = {props.handleClick}/>);
   return (
     <div className="video-list">
-      <div><h5><em>videoListEntry</em>
-        {props.videos.map((video) => (
-          <VideoListEntry video={video} key= {video.id.videoId}/>
-        ))}
-      </h5></div>
+      {vList}
     </div>
   );
+
 };
 
 // PropTypes tell other developers what `props` a component expects
